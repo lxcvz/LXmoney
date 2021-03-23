24307fd1617190f20dd74ae0effe2e81c6f9059f
Lucas <lucas-mateus.dc@hotmail.com>
2021-03-23 17:58:48 -0300

    initial commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+        .then(response => console.log(response.data))
+    }, []);
+
+    return(
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>21/03/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Lanches</td>
+                        <td className="withdraw">- R$50,00</td>
+                        <td>Alimentacão</td>
+                        <td>21/03/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+
+    );
+}
